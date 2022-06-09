29d16018dbf31ce8f0e7f4b6dfdfd7c4e1b44815
edgar <ealejor@fcpn.edu.bo>
2022-06-09 10:43:11 -0400

    update

--- src/ejercicio1/Factura.java
+++ src/ejercicio1/Factura.java
@@ -1,4 +1,27 @@
 package ejercicio1;
 
 public class Factura {
+    private int nit;
+
+    public Factura(int nit) {
+        this.nit = nit;
+    }
+
+    public Factura() {
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "nit=" + nit +
+                '}';
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
 }
