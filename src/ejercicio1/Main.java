b6608b461a5429a0d3d282164c6c8acf1287f9dc
edgar <ealejor@fcpn.edu.bo>
2022-06-09 10:53:56 -0400

    update

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -2,7 +2,13 @@ package ejercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("world");
+        System.out.println("\nInstanciar 2 obj. Factura con 2 y 3 productos respectivamente y mostrar datos.");
+        Item p1 = new Item("Coca cola", 10.0);
+        Item p2 = new Item("Pepsi", 12);
+        Item p3 = new Item("tv", 5000);
+        Item p4 = new Item("Lavadora", 9000);
+        Item p5 = new Item("Sprite", 2);
+
+        Factura factura1 = new Factura();
     }
 }
