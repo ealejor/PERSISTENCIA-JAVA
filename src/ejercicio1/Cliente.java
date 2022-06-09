5bfff71cd5be991fa1582067dcecec6ec68d503c
edgar <ealejor@fcpn.edu.bo>
2022-06-09 10:41:26 -0400

    update

--- src/ejercicio1/Cliente.java
+++ src/ejercicio1/Cliente.java
@@ -1,4 +1,37 @@
 package ejercicio1;
 
 public class Cliente {
+    private String nom;
+    private int ci;
+
+    public Cliente(String nom, int ci) {
+        this.nom = nom;
+        this.ci = ci;
+    }
+
+    public Cliente(){}
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nom='" + nom + '\'' +
+                ", ci=" + ci +
+                '}';
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public void setCi(int ci) {
+        this.ci = ci;
+    }
 }
