eb7a3216d049f5f2b12dc9630c9cefbbaa2e684f
edgar <ealejor@fcpn.edu.bo>
2022-06-10 11:08:07 -0400

    update

--- src/liberation/Libro.java
+++ src/liberation/Libro.java
@@ -1,4 +1,69 @@
 package liberation;
 
 public class Libro {
+    private String nombre;
+    private String autor;
+    private int nroPag;
+    private String codigo;
+    private String estado;
+
+    public Libro(String nombre, String autor, int nroPag, String codigo, String estado) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.nroPag = nroPag;
+        this.codigo = codigo;
+        this.estado = estado;
+    }
+    public Libro(){}
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "nombre='" + nombre + '\'' +
+                ", autor='" + autor + '\'' +
+                ", nroPag=" + nroPag +
+                ", codigo='" + codigo + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNroPag() {
+        return nroPag;
+    }
+
+    public void setNroPag(int nroPag) {
+        this.nroPag = nroPag;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- src/liberation/Prestamo.java
+++ src/liberation/Prestamo.java
@@ -1,4 +1,94 @@
 package liberation;
 
+import java.util.Arrays;
+
 public class Prestamo {
+    private Libro[] L;
+    private int ci;
+    private int nroLibros;
+    private String nombreLector;
+    private String fechaPrestamo;
+    private String fechaDevolucion;
+
+    public Prestamo(int ci, int nroLibros, String nombreLector, String fechaPrestamo, String fechaDevolucion) {
+        L = new Libro[nroLibros];
+        this.ci = ci;
+        this.nroLibros = nroLibros;
+        this.nombreLector = nombreLector;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Prestamo() {
+    }
+
+    public void mostrar() {
+        String a = "";
+        a += "Prestamo{ci=" + ci + ", nroLibros=" + nroLibros + ", nombreLector=" + nombreLector + ", fechaPrestamo=" + fechaPrestamo + ", fechaDevolucion=" + fechaDevolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "L=" + Arrays.toString(L) +
+                ", ci=" + ci +
+                ", nroLibros=" + nroLibros +
+                ", nombreLector='" + nombreLector + '\'' +
+                ", fechaPrestamo='" + fechaPrestamo + '\'' +
+                ", fechaDevolucion='" + fechaDevolucion + '\'' +
+                '}';
+    }
+
+    public Libro[] getL() {
+        return L;
+    }
+
+    public void setL(Libro l) {
+        for (int i = 0; i < nroLibros; i++) {
+            if (L[i] == null) {
+                L[i] = l;
+                break;
+            }
+        }
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public void setCi(int ci) {
+        this.ci = ci;
+    }
+
+    public int getNroLibros() {
+        return nroLibros;
+    }
+
+    public void setNroLibros(int nroLibros) {
+        this.nroLibros = nroLibros;
+    }
+
+    public String getNombreLector() {
+        return nombreLector;
+    }
+
+    public void setNombreLector(String nombreLector) {
+        this.nombreLector = nombreLector;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
 }
